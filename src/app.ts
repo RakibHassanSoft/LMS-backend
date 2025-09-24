26d968f28f521acf71e3a7307528ee7199496a24
RakibHassanSoft <rakibreings32@gmail.com>
2025-09-24 14:34:52 +0600

    Ts backend basic project initilized

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express,{ Application, Request, Response } from "express";
+
+
+const app : Application = express();
+
+app.get("/",(req : Request,res : Response)=>{
+    res.send("Hello World");
+});
+
+app.use(express.json());
+
+app.listen(5000,()=>{
+    console.log("Server is running on port 3000");
+});
